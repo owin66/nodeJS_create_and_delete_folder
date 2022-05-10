ca0dd4b33ef6a8f6e48a88867bbbceb76d01c0ef
owin66 <78900270+owin66@users.noreply.github.com>
2022-05-10 09:42:54 +0300

    node_unlink_and_rmdir(delete_files_and_folders)

--- app.js
+++ app.js
@@ -9,3 +9,17 @@ fs.readFile('./test.txt', 'utf8', (error, data) => {
         });
     });
 })
+
+setTimeout(() => {
+    if(fs.existsSync('./files/test2.txt')) {
+        fs.unlink('./files/test2.txt', () => {
+        })
+    }
+}, 4000)
+
+setTimeout(() => {
+    if(fs.existsSync('./files')) {
+        fs.rmdir('./files', () => {
+        })
+    }
+}, 6000)
